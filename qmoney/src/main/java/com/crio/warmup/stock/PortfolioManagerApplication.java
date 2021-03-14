bac6775eed322798cc22bef8480f55e7e11eef99
roby <robyjacob>
2021-03-14 19:31:25 +0530

    qmoney rest api

--- qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -1,9 +1,10 @@
 
 package com.crio.warmup.stock;
 
-
 import com.crio.warmup.stock.dto.AnnualizedReturn;
+import com.crio.warmup.stock.dto.Candle;
 import com.crio.warmup.stock.dto.PortfolioTrade;
+import com.crio.warmup.stock.dto.TiingoCandle;
 import com.crio.warmup.stock.dto.TotalReturnsDto;
 import com.crio.warmup.stock.log.UncaughtExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +20,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,18 +31,24 @@ import java.util.stream.Stream;
 import org.apache.logging.log4j.ThreadContext;
 import org.springframework.web.client.RestTemplate;
 
-
 public class PortfolioManagerApplication {
 
-  // TODO: CRIO_TASK_MODULE_JSON_PARSING
-  //  Read the json file provided in the argument[0]. The file will be available in the classpath.
-  //    1. Use #resolveFileFromResources to get actual file from classpath.
-  //    2. Extract stock symbols from the json file with ObjectMapper provided by #getObjectMapper.
-  //    3. Return the list of all symbols in the same order as provided in json.
+  private static RestTemplate restTemplate = new RestTemplate();
+  private static final String API_TOKEN = "4dd7b0eb5e86af400d5c10e372ab0a2e49ec7bfb";
+  private static String apiUrl = "https://api.tiingo.com/tiingo/daily/{sym}/prices?startDate={startDate}&endDate={endDate}&token={token}";
 
-  //  Note:
-  //  1. There can be few unused imports, you will need to fix them to make the build pass.
-  //  2. You can use "./gradlew build" to check if your code builds successfully.
+  // TODO: CRIO_TASK_MODULE_JSON_PARSING
+  // Read the json file provided in the argument[0]. The file will be available in
+  // the classpath.
+  // 1. Use #resolveFileFromResources to get actual file from classpath.
+  // 2. Extract stock symbols from the json file with ObjectMapper provided by
+  // #getObjectMapper.
+  // 3. Return the list of all symbols in the same order as provided in json.
+
+  // Note:
+  // 1. There can be few unused imports, you will need to fix them to make the
+  // build pass.
+  // 2. You can use "./gradlew build" to check if your code builds successfully.
 
   public static List<String> mainReadFile(String[] args) throws IOException, URISyntaxException {
     File tradeFile = resolveFileFromResources(args[0]);
@@ -48,33 +57,97 @@ public class PortfolioManagerApplication {
 
     PortfolioTrade[] trades = mapper.readValue(tradeFile, PortfolioTrade[].class);
 
-    for (PortfolioTrade trade: trades) {
+    for (PortfolioTrade trade : trades) {
       symbols.add(trade.getSymbol());
     }
 
     return symbols;
   }
 
+  private static PortfolioTrade[] getTrades(String fileName) throws IOException, URISyntaxException {
+    File tradeFile = resolveFileFromResources(fileName);
+    ObjectMapper mapper = getObjectMapper();
+    List<String> symbols = new ArrayList<>();
+
+    PortfolioTrade[] trades = mapper.readValue(tradeFile, PortfolioTrade[].class);
+
+    return trades;
+  }
+
+  private static List<Candle> getQuotes(String sym, LocalDate startDate, String endDate) {
+    Map<String, String> map = new HashMap<>();
+    map.put("sym", sym);
+    map.put("token", API_TOKEN);
+    map.put("endDate", endDate);
+    map.put("startDate", startDate.toString());
+
+    if (LocalDate.parse(endDate).isBefore(startDate)) throw new RuntimeException();
+
+    Candle[] quotes = restTemplate.getForObject(apiUrl, TiingoCandle[].class, map);
+
+    return Arrays.asList(quotes);
+  }
+
+  private static List<String> sortSymbols(List<Map.Entry<String, Double>> symbolClose) {
+    List<String> sortedSymbols = new ArrayList<>();
+
+    Collections.sort(symbolClose, new Comparator<Map.Entry<String, Double>>() {
+      @Override
+      public int compare(Map.Entry<String, Double> e1, Map.Entry<String, Double> e2) {
+        return e1.getValue().compareTo(e2.getValue());
+      }
+    });
+
+    for (Map.Entry<String, Double> entry : symbolClose) {
+      sortedSymbols.add(entry.getKey());
+    }
+
+    return sortedSymbols;
+  }
 
   // Note:
   // 1. You may need to copy relevant code from #mainReadQuotes to parse the Json.
   // 2. Remember to get the latest quotes from Tiingo API.
 
-
-
-
   // TODO: CRIO_TASK_MODULE_REST_API
-  //  Find out the closing price of each stock on the end_date and return the list
-  //  of all symbols in ascending order by its close value on end date.
+  // Find out the closing price of each stock on the end_date and return the list
+  // of all symbols in ascending order by its close value on end date.
 
   // Note:
   // 1. You may have to register on Tiingo to get the api_token.
   // 2. Look at args parameter and the module instructions carefully.
   // 2. You can copy relevant code from #mainReadFile to parse the Json.
   // 3. Use RestTemplate#getForObject in order to call the API,
-  //    and deserialize the results in List<Candle>
+  // and deserialize the results in List<Candle>
 
+  public static List<String> mainReadQuotes(String[] args) throws IOException, URISyntaxException {
+    PortfolioTrade[] trades = getTrades(args[0]);
+    List<String> symbols = new ArrayList<>();
+    List<LocalDate> startDates = new ArrayList<>();
+    String endDate = args[1];
+    Map<String, Double> symCloseVal = new HashMap<>();
 
+    for (PortfolioTrade trade: trades) {
+      symbols.add(trade.getSymbol());
+      startDates.add(trade.getPurchaseDate());
+    }
+
+    for (int i = 0; i < trades.length; i++) {
+      String symbol = symbols.get(i);
+      LocalDate startDate = startDates.get(i);
+      List<Candle> candles = getQuotes(symbol, startDate, endDate);
+
+      for (Candle candle : candles) {
+        symCloseVal.put(symbol, candle.getClose());
+      }
+    }
+
+    return sortSymbols(new ArrayList<>(symCloseVal.entrySet()));
+  }
+
+  // Note:
+  // Remember to confirm that you are getting same results for annualized returns
+  // as in Module 3.
 
   private static void printJsonObject(Object object) throws IOException {
     Logger logger = Logger.getLogger(PortfolioManagerApplication.class.getCanonicalName());
@@ -83,8 +156,7 @@ public class PortfolioManagerApplication {
   }
 
   private static File resolveFileFromResources(String filename) throws URISyntaxException {
-    return Paths.get(
-        Thread.currentThread().getContextClassLoader().getResource(filename).toURI()).toFile();
+    return Paths.get(Thread.currentThread().getContextClassLoader().getResource(filename).toURI()).toFile();
   }
 
   private static ObjectMapper getObjectMapper() {
@@ -93,73 +165,56 @@ public class PortfolioManagerApplication {
     return objectMapper;
   }
 
-
   // TODO: CRIO_TASK_MODULE_JSON_PARSING
-  //  Follow the instructions provided in the task documentation and fill up the correct values for
-  //  the variables provided. First value is provided for your reference.
-  //  A. Put a breakpoint on the first line inside mainReadFile() which says
-  //    return Collections.emptyList();
-  //  B. Then Debug the test #mainReadFile provided in PortfoliomanagerApplicationTest.java
-  //  following the instructions to run the test.
-  //  Once you are able to run the test, perform following tasks and record the output as a
-  //  String in the function below.
-  //  Use this link to see how to evaluate expressions -
-  //  https://code.visualstudio.com/docs/editor/debugging#_data-inspection
-  //  1. evaluate the value of "args[0]" and set the value
-  //     to the variable named valueOfArgument0 (This is implemented for your reference.)
-  //  2. In the same window, evaluate the value of expression below and set it
-  //  to resultOfResolveFilePathArgs0
-  //     expression ==> resolveFileFromResources(args[0])
-  //  3. In the same window, evaluate the value of expression below and set it
-  //  to toStringOfObjectMapper.
-  //  You might see some garbage numbers in the output. Dont worry, its expected.
-  //    expression ==> getObjectMapper().toString()
-  //  4. Now Go to the debug window and open stack trace. Put the name of the function you see at
-  //  second place from top to variable functionNameFromTestFileInStackTrace
-  //  5. In the same window, you will see the line number of the function in the stack trace window.
-  //  assign the same to lineNumberFromTestFileInStackTrace
-  //  Once you are done with above, just run the corresponding test and
-  //  make sure its working as expected. use below command to do the same.
-  //  ./gradlew test --tests PortfolioManagerApplicationTest.testDebugValues
+  // Follow the instructions provided in the task documentation and fill up the
+  // correct values for
+  // the variables provided. First value is provided for your reference.
+  // A. Put a breakpoint on the first line inside mainReadFile() which says
+  // return Collections.emptyList();
+  // B. Then Debug the test #mainReadFile provided in
+  // PortfoliomanagerApplicationTest.java
+  // following the instructions to run the test.
+  // Once you are able to run the test, perform following tasks and record the
+  // output as a
+  // String in the function below.
+  // Use this link to see how to evaluate expressions -
+  // https://code.visualstudio.com/docs/editor/debugging#_data-inspection
+  // 1. evaluate the value of "args[0]" and set the value
+  // to the variable named valueOfArgument0 (This is implemented for your
+  // reference.)
+  // 2. In the same window, evaluate the value of expression below and set it
+  // to resultOfResolveFilePathArgs0
+  // expression ==> resolveFileFromResources(args[0])
+  // 3. In the same window, evaluate the value of expression below and set it
+  // to toStringOfObjectMapper.
+  // You might see some garbage numbers in the output. Dont worry, its expected.
+  // expression ==> getObjectMapper().toString()
+  // 4. Now Go to the debug window and open stack trace. Put the name of the
+  // function you see at
+  // second place from top to variable functionNameFromTestFileInStackTrace
+  // 5. In the same window, you will see the line number of the function in the
+  // stack trace window.
+  // assign the same to lineNumberFromTestFileInStackTrace
+  // Once you are done with above, just run the corresponding test and
+  // make sure its working as expected. use below command to do the same.
+  // ./gradlew test --tests PortfolioManagerApplicationTest.testDebugValues
 
   public static List<String> debugOutputs() {
 
-     String valueOfArgument0 = "trades.json";
-     String resultOfResolveFilePathArgs0 = "/home/roby/crio-proj/qmoney/qmoney/bin/main/trades.json";
-     String toStringOfObjectMapper = "com.fasterxml.jackson.databind.ObjectMapper@797b0699";
-     String functionNameFromTestFileInStackTrace = "PortfolioManagerApplication.main(String[])";
-     String lineNumberFromTestFileInStackTrace = "135:1";
+    String valueOfArgument0 = "trades.json";
+    String resultOfResolveFilePathArgs0 = "/home/roby/crio-proj/qmoney/qmoney/bin/main/trades.json";
+    String toStringOfObjectMapper = "com.fasterxml.jackson.databind.ObjectMapper@797b0699";
+    String functionNameFromTestFileInStackTrace = "PortfolioManagerApplication.main(String[])";
+    String lineNumberFromTestFileInStackTrace = "135:1";
 
-
-    return Arrays.asList(new String[]{valueOfArgument0, resultOfResolveFilePathArgs0,
-        toStringOfObjectMapper, functionNameFromTestFileInStackTrace,
-        lineNumberFromTestFileInStackTrace});
+    return Arrays.asList(new String[] { valueOfArgument0, resultOfResolveFilePathArgs0, toStringOfObjectMapper,
+        functionNameFromTestFileInStackTrace, lineNumberFromTestFileInStackTrace });
   }
-
-
-  // Note:
-  // Remember to confirm that you are getting same results for annualized returns as in Module 3.
-  public static List<String> mainReadQuotes(String[] args) throws IOException, URISyntaxException {
-     return Collections.emptyList();
-  }
-
-
-
-
-
-
-
-
-
 
   public static void main(String[] args) throws Exception {
     Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
     ThreadContext.put("runId", UUID.randomUUID().toString());
 
-
     printJsonObject(mainReadQuotes(args));
-
-
   }
 }
-
