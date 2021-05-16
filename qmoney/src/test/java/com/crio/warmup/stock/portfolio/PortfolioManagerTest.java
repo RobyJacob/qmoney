cc6aa3d9e228b80f968a402b4dd0bc94870199df
Roby Jacob <robyjacob@Robys-Mac-mini.local>
2021-05-16 17:08:56 +0530

    removed unwanted test cases

--- qmoney/src/test/java/com/crio/warmup/stock/portfolio/PortfolioManagerTest.java
+++ qmoney/src/test/java/com/crio/warmup/stock/portfolio/PortfolioManagerTest.java
@@ -79,13 +79,16 @@ class PortfolioManagerTest {
     @Test
     public void calculateExtrapolatedAnnualizedReturn() throws Exception {
         // given
-        String moduleToRun = null;
+        String moduleToRun = "ADDITIONAL_REFACTOR";
 
-        if (moduleToRun.equals("REFACTOR")) {
-            Mockito.doReturn(getCandles(aaplQuotes)).when(portfolioManager).getStockQuote(eq("AAPL"), any(), any());
-            Mockito.doReturn(getCandles(msftQuotes)).when(portfolioManager).getStockQuote(eq("MSFT"), any(), any());
-            Mockito.doReturn(getCandles(googlQuotes)).when(portfolioManager).getStockQuote(eq("GOOGL"), any(), any());
-        }
+        // if (moduleToRun.equals("REFACTOR")) {
+        // Mockito.doReturn(getCandles(aaplQuotes)).when(portfolioManager).getStockQuote(eq("AAPL"),
+        // any(), any());
+        // Mockito.doReturn(getCandles(msftQuotes)).when(portfolioManager).getStockQuote(eq("MSFT"),
+        // any(), any());
+        // Mockito.doReturn(getCandles(googlQuotes)).when(portfolioManager).getStockQuote(eq("GOOGL"),
+        // any(), any());
+        // }
         PortfolioTrade trade1 = new PortfolioTrade("AAPL", 50, LocalDate.parse("2019-01-02"));
         PortfolioTrade trade2 = new PortfolioTrade("GOOGL", 100, LocalDate.parse("2019-01-02"));
         PortfolioTrade trade3 = new PortfolioTrade("MSFT", 20, LocalDate.parse("2019-01-02"));
